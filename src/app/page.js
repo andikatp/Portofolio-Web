9fdd43ed002ea166807871df4282d0881bf33b74
andikatp <triprasetya_andika@yahoo.com>
2024-01-24 21:49:58 +0700

    update

---
+++ src/app/page.js
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+
+  return (
+    <main>
+      {/* <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Landing />
+      <Description />
+      <Projects />
+      <SlidingImages />
+      <Contact /> */}
+    </main>
+  );
+}
